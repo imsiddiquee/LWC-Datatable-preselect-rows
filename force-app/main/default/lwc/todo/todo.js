d2ef477467a9bda448bf0595a8a4760f070d374c
unknown <imsiddiquee@gmail.com>
2021-05-27 10:45:33 +0600

    store all information in js list

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,3 +1,29 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
-export default class Todo extends LightningElement {}
+export default class Todo extends LightningElement {
+  @track
+  todoTasks = [
+    { id: 1, name: "Task 1" },
+    { id: 2, name: "Task 2" },
+    { id: 3, name: "Task 3" }
+  ];
+  newTask = "My new task";
+
+  updateNewTask(event) {
+    this.newTask = event.target.value;
+    console.log(this.newTask);
+  }
+
+  addTaskToList() {
+    this.todoTasks.push({ id: this.todoTasks.length + 1, name: this.newTask });
+    this.newTask = "";
+  }
+
+  deleteTaskFromList(event) {
+    const idToDelete = event.target.name;
+    let todoTasks = this.todoTasks;
+    const todoTaskIndex = todoTasks.findIndex((task) => task.id === idToDelete);
+
+    todoTasks.splice(todoTaskIndex, 1);
+  }
+}
