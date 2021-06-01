8f33d5bf3ab051c7ad31e82be31a09dcd1d572b6
Shamim <imsiddiquee@gmail.com>
2021-06-01 19:34:38 +0600

    update

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,13 +1,37 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, track, wire } from "lwc";
+import { refreshApex } from "@salesforce/apex";
+import getTasks from "@salesforce/apex/ToDoListController.getTasks";
+import insertTask from "@salesforce/apex/ToDoListController.insertTask";
+import deleteTask from "@salesforce/apex/ToDoListController.deleteTask";
 
 export default class Todo extends LightningElement {
   @track
   todoTasks = [
-    { id: 1, name: "Task 1" },
-    { id: 2, name: "Task 2" },
-    { id: 3, name: "Task 3" }
+    // { id: 1, name: "Task 1" },
+    // { id: 2, name: "Task 2" },
+    // { id: 3, name: "Task 3" }
   ];
-  newTask = "My new task";
+  todoTasksResponse;
+  newTask = "";
+
+  @wire(getTasks) getTodoTask(response) {
+    this.todoTasksResponse = response;
+    let data = response.data;
+    let error = response.error;
+    if (data) {
+      console.log("data");
+      this.todoTasks = [];
+      data.forEach((task) => {
+        this.todoTasks.push({
+          id: this.todoTasks.length + 1,
+          name: task.Subject,
+          recordId: task.Id
+        });
+      });
+    } else if (error) {
+      console.log("error");
+    }
+  }
 
   updateNewTask(event) {
     this.newTask = event.target.value;
@@ -15,15 +39,47 @@ export default class Todo extends LightningElement {
   }
 
   addTaskToList() {
-    this.todoTasks.push({ id: this.todoTasks.length + 1, name: this.newTask });
-    this.newTask = "";
+    // console.log("this.todoTasks:::", JSON.stringify(this.todoTasks));
+    // this.todoTasks.push({ id: this.todoTasks.length + 1, name: this.newTask });
+    // this.newTask = "";
+
+    insertTask({ subject: this.newTask })
+      .then((response) => {
+        // this.accountRecords = response;
+
+        this.todoTasks.push({
+          id: this.todoTasks.length + 1,
+          name: this.newTask,
+          recordId: response.Id
+        });
+        this.newTask = "";
+      })
+      .catch((error) => {
+        console.log(error.body.message);
+      });
   }
 
   deleteTaskFromList(event) {
     const idToDelete = event.target.name;
     let todoTasks = this.todoTasks;
+    let recordIdToDelete;
     const todoTaskIndex = todoTasks.findIndex((task) => task.id === idToDelete);
 
-    todoTasks.splice(todoTaskIndex, 1);
+    recordIdToDelete = this.todoTasks[todoTaskIndex].recordId;
+    deleteTask({ recordId: recordIdToDelete })
+      .then((response) => {
+        if (response) {
+          todoTasks.splice(todoTaskIndex, 1);
+        } else {
+          console.log("Unable to delete task.");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  refreshTotoList() {
+    refreshApex(this.todoTasksResponse);
   }
 }
